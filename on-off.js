6dc4a6586e4dbe25829932be3dc1dfe14cc0b40a
Antony Amal Rekshin <antonyamalrekshin@gmail.com>
2025-01-09 09:46:37 +0530

    on-off.js

--- on-off.js
+++ on-off.js
@@ -2,6 +2,7 @@ const Powerbtn = document.getElementById("switchBtn");
 const display = document.getElementById("displayed");
 const dots = document.getElementsByClassName("dot");
 const offBtn = document.getElementById("switchOff");
+const offDisBtn = document.querySelector("#displayed h1");
 
 Powerbtn.addEventListener("click", () => {
     const currentBgColor = getComputedStyle(display).backgroundColor;
@@ -11,6 +12,8 @@ Powerbtn.addEventListener("click", () => {
         display.style.backgroundColor = "transparent";
         Powerbtn.textContent = "ON";
         Powerbtn.style.backgroundColor = "green";
+        offDisBtn.classList.remove("animate")
+        
 
     Array.from(dots).forEach((dot) => {
         dot.classList.remove("hidden");
@@ -21,6 +24,7 @@ Powerbtn.addEventListener("click", () => {
         display.style.backgroundColor = "rgb(0, 0, 0)";
         Powerbtn.textContent = "OFF";
         Powerbtn.style.backgroundColor = "red";
+        offDisBtn.classList.add("animate")
 
     Array.from(dots).forEach((dot) => {
         dot.classList.remove("visible");
